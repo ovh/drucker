f65332c40e4222a0c8a0850d3be7bb46b602467f
frisouilles <kevin.bonduelle@corp.ovh.com>
2017-09-25 13:41:22 +0000

    Travis #8

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 var navbarButton = document.getElementById('navbar-toggle');
 var navMain = document.getElementById('navbar-entries');
+var navIcon = document.getElementById('navbar-icon');
 var navbarToggle = false;
 
 var toggleNav = function() {
@@ -7,9 +8,11 @@ var toggleNav = function() {
 
     if (navbarToggle) {
         navMain.classList.add('navbar__entries--active');
+        navIcon.classList.add('navbar__icon--active');
     }
     else {
         navMain.classList.remove('navbar__entries--active');
+        navIcon.classList.remove('navbar__icon--active');
     }
 }
 
